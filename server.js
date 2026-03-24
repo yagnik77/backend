3558d3e3a580f06546fa2c7b763f210bbac766d5
yagnik77 <777yagnik@gmail.com>
2026-03-24 16:52:20 +0530

    new add

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
+
 
 app.post("/api/chat", async (req, res) => {
   try {
